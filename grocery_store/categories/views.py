6917bd0873f5ae1cc77121aacb17dea4506186be
Glebchik57 <g.sevostyanov@inbox.ru>
2024-05-25 17:01:58 +0200

    viewsets for cat and subcat were created

--- grocery_store/categories/views.py
+++ grocery_store/categories/views.py
@@ -1,3 +1,18 @@
-from django.shortcuts import render
+from rest_framework import viewsets
+from django.contrib.auth import get_user_model
 
-# Create your views here.
+from .serializers import CategoriesSerializer, SubCategoriesSerializer
+from .models import Categories, SubCategories
+from .permissions import IsAdminOrReadOnly
+
+
+class CategoriesViewSet(viewsets.ModelViewSet):
+    queryset = Categories.objects.all()
+    serializer_class = CategoriesSerializer
+    permission_classes = (IsAdminOrReadOnly,)
+
+
+class SubCategoriesViewSet(viewsets.ModelViewSet):
+    queryset = SubCategories.objects.all()
+    serializer_class = SubCategoriesSerializer
+    permission_classes = (IsAdminOrReadOnly,)
