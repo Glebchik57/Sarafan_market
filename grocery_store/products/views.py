cb42a8ca6d86471b2cb85f438cea2095e0d14a59
Glebchik57 <g.sevostyanov@inbox.ru>
2024-05-25 18:07:15 +0200

    viewsets products and basket were created

--- grocery_store/products/views.py
+++ grocery_store/products/views.py
@@ -1,3 +1,28 @@
-from django.shortcuts import render
+from rest_framework import viewsets
+from rest_framework.decorators import action
 
-# Create your views here.
+from .models import Products, Basket
+from .serializers import ProductsSerializer, BasketSerializer
+from categories.permissions import IsAdminOrReadOnly
+from .permission import IsOwner
+
+
+class ProductsSerializer(viewsets.ModelViewSet):
+    queryset = Products.objects.all()
+    serializer_class = ProductsSerializer
+    permission_classes = (IsAdminOrReadOnly,)
+
+
+class BasketViewSet(viewsets.ModelViewSet):
+    queryset = Basket.objects.all()
+    serializer_class = BasketSerializer
+    permission_classes = (IsOwner,)
+
+    @action(
+        ['DELETE'],
+        detail=False,
+        url_path='clear_basket',
+    )
+    def clear_basket(self, request):
+        products = Basket.products_to_basket.all()
+        products.delete()
