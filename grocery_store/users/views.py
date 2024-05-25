fddc995df477858d3dda35ac3563509a187c23e8
Glebchik57 <g.sevostyanov@inbox.ru>
2024-05-25 16:18:55 +0200

    user viewset was created

--- grocery_store/users/views.py
+++ grocery_store/users/views.py
@@ -1,3 +1,14 @@
-from django.shortcuts import render
+from rest_framework import viewsets
+from djoser.views import UserViewSet
+from django.contrib.auth import get_user_model
+from rest_framework.permissions import AllowAny
 
-# Create your views here.
+from .serializers import UserSerializer
+
+User = get_user_model()
+
+
+class CustomUserViewSet(UserViewSet):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
+    permission_classes = (AllowAny,)
