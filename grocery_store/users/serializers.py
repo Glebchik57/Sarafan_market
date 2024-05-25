9d06f0f6b2108b777a27b7051f412b13e1c5f0d1
Glebchik57 <g.sevostyanov@inbox.ru>
2024-05-25 20:01:45 +0200

    user`s serializers were corrected

--- grocery_store/users/serializers.py
+++ grocery_store/users/serializers.py
@@ -1,10 +1,18 @@
 from rest_framework import serializers
 from django.contrib.auth import get_user_model
+from djoser.serializers import UserSerializer, UserCreateSerializer
 
 User = get_user_model()
 
 
-class UserSerializer(serializers.ModelSerializer):
+class UserSerializer(UserSerializer):
+
+    class Meta:
+        model = User
+        fields = ('username', 'email')
+
+
+class UserCreateSerializer(UserCreateSerializer):
 
     class Meta:
         model = User
