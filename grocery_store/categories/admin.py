63dcde1ce4e3d071b541b213ba6ead8ee4646e6a
Glebchik57 <g.sevostyanov@inbox.ru>
2024-05-26 11:28:47 +0200

    filtres were deleted

--- grocery_store/categories/admin.py
+++ grocery_store/categories/admin.py
@@ -1,3 +1,18 @@
 from django.contrib import admin
+from .models import Categories, SubCategories
 
-# Register your models here.
+
+class CategoriesAdmin(admin.ModelAdmin):
+    list_display = ('name', 'slug', 'image',)
+    search_fields = ('name',)
+    list_filter = ('id',)
+
+
+class SubCategoriesAdmin(admin.ModelAdmin):
+    list_display = ('name', 'slug', 'category', 'image',)
+    search_fields = ('name',)
+    list_filter = ('id',)
+
+
+admin.site.register(Categories, CategoriesAdmin)
+admin.site.register(SubCategories, SubCategoriesAdmin)
